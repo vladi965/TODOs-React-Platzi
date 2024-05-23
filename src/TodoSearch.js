eb8f45e6e5437afb2ad83335177701d6763bdc18
vladi965 <manolomunoz0596@gmail.com>
2024-05-23 14:26:15 -0500

    Aun falta terminar

---
+++ src/TodoSearch.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./TodoSearch.css";
+
+function TodoSearch({ searchValue, setSearchValue }) {
+
+    return (
+        <input
+            placeholder="Buscar TODOs"
+            className="TodoSearch"
+            value={searchValue}
+            onChange={(event) => {
+                setSearchValue(event.target.value);
+            }}
+        />
+    );
+}
+
+export { TodoSearch };
